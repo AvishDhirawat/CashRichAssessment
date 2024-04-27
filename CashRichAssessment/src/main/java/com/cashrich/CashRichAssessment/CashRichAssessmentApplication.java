64c960adf9a724923c3112ae3397218b01202869
Avish Dhirawat <31956212+AvishDhirawat@users.noreply.github.com>
2024-04-27 16:02:07 +0530

    Add files via upload

---
+++ CashRichAssessment/src/main/java/com/cashrich/CashRichAssessment/CashRichAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.cashrich.CashRichAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRichAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashRichAssessmentApplication.class, args);
+	}
+
+}
